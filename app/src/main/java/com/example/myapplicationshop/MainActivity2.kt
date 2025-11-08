616fe147ce54442e07e88176eca3e0f3eb35a802
Kiketka <stenka.moskalenko@yandex.ru>
2025-11-08 19:39:44 +0300

    Catalog mark2

--- app/src/main/java/com/example/myapplicationshop/MainActivity2.kt
+++ app/src/main/java/com/example/myapplicationshop/MainActivity2.kt
@@ -1,12 +1,21 @@
 package com.example.myapplicationshop
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import Produkt
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+
+
+private val products = listOf(
+    Produkt(id=1, name="Франшиза1", price = 5000000.0, ImageRes =R.drawable.one ),
+    Produkt(id=2, name="Франшиза2", price = 8500000.0, ImageRes =R.drawable.two ),
+    Produkt(id=3, name="Франшиза3", price = 6000000.0, ImageRes =R.drawable.thre ),
+    Produkt(id=4, name="Франшиза4", price = 2000000.0, ImageRes =R.drawable.four ),
+    Produkt(id=5, name="Франшиза5", price = 2500000.0, ImageRes =R.drawable.five ),
+)
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,12 +23,17 @@ class MainActivity2 : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.`activity_sek`)
 
-        val btn_Stop = findViewById<Button>(R.id.btntwo)
+    val container = findViewById<LinearLayout>(R.id.Catalog)
 
-        btn_Stop.setOnClickListener {
-            //Toast.makeText(this, "Страница временно не работает", Toast.LENGTH_LONG).show()
-            val intent = Intent(this, MainActivity::class.java )
-            startActivity(intent)
+        products.forEach{ produkt ->
+            val view = layoutInflater.inflate(R.layout.item_product,container, false)
+
+            view.findViewById<ImageView>(R.id.ivProductImage).setImageResource(produkt.ImageRes)
+            view.findViewById<TextView>(R.id.tvProductName).text = produkt.name
+            view.findViewById<TextView>(R.id.tvProductPrise).text = "${produkt.price} ₽"
+
+            container.addView(view)
         }
+
     }
 }
