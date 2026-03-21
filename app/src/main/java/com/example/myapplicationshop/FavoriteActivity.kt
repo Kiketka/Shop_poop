a69f2d5a6a33d44b72488d2d74eda9512bf1e989
Kiketka <stenka.moskalenko@yandex.ru>
2026-03-21 19:19:43 +0300

    History 13.0

---
+++ app/src/main/java/com/example/myapplicationshop/FavoriteActivity.kt
@@ -0,0 +1,20 @@
+package com.example.myapplicationshop
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplicationshop.model.CartStorage
+import android.content.Intent
+import androidx.activity.enableEdgeToEdge
+import com.example.myapplicationshop.model.HistoryStorage
+import com.example.myapplicationshop.model.Order
+import java.util.Date
+import java.util.Locale.getDefault
+
+class FavoriteActivity {
+
+}
